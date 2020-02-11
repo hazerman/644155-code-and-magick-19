d4c9505ca607dd677896b13a630e723de32084b5
hazerman <49827421+hazerman@users.noreply.github.com>
2020-02-11 22:13:27 +0500

    Добавляет ограничения перемещения диалогового окна

--- js/drag-n-drop.js
+++ js/drag-n-drop.js
@@ -4,14 +4,24 @@
   var userDialog = document.querySelector('.setup');
   var uploadWrapper = userDialog.querySelector('.upload');
 
+
   uploadWrapper.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
+    var AreaLimit = {
+      X: {
+        MIN: userDialog.offsetWidth / 2,
+        MAX: userDialog.parentElement.clientWidth - userDialog.offsetWidth / 2
+      },
+      Y: {
+        MIN: 0,
+        MAX: document.documentElement.clientHeight - 100
+      }
+    };
     var startCoordinates = {
       x: evt.clientX,
       y: evt.clientY
     };
     var isDragged = false;
-
     var uploadWrapperMouseMoveHandler = function (moveEvt) {
       moveEvt.preventDefault();
       isDragged = true;
@@ -23,8 +33,25 @@
         x: moveEvt.clientX,
         y: moveEvt.clientY
       };
-      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
-      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+      var newStyleLeft = (userDialog.offsetLeft - shift.x);
+      var newStyleTop = (userDialog.offsetTop - shift.y);
+
+      var getPositionStyle = function (axis, newStylePosition) {
+        var position;
+        if (newStylePosition < (AreaLimit[axis].MIN)) {
+          position = (AreaLimit[axis].MIN) + 'px';
+          document.removeEventListener('mousemove', uploadWrapperMouseMoveHandler);
+        } else if (newStylePosition > (AreaLimit[axis].MAX)) {
+          position = (AreaLimit[axis].MAX) + 'px';
+          document.removeEventListener('mousemove', uploadWrapperMouseMoveHandler);
+        } else {
+          position = newStylePosition + 'px';
+        }
+        return position;
+      };
+
+      userDialog.style.left = getPositionStyle('X', newStyleLeft);
+      userDialog.style.top = getPositionStyle('Y', newStyleTop);
     };
 
     var uploadWrapperMouseUpHandler = function (upEvt) {
